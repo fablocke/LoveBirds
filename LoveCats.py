bec1d27f8cd6f2acfb11b8b9912988a341cbd224
fbeck <Fabian.Beck@posteo.de>
2020-02-12 11:03:02 +0100

    first push

--- LoveCats.py
+++ LoveCats.py
@@ -1,7 +1,4 @@
-#credit to Louis Ros
-
-
-#!/usr/bin/python3.5
+##!/usr/bin/python3
 import asyncio
 import time
 import os
@@ -12,7 +9,12 @@ from telethon.tl.types import InputMessagesFilterVoice
 import RPi.GPIO as GPIO
 from gpiozero import Servo
 from time import sleep
+from neopixel import *
+from random import randrange
 
+
+phone = '' #change this: This is your phone number
+peer = '@' #and this: This is the username that you want to talk with
 
 """
 initialisation of GPIOs
@@ -32,6 +34,17 @@ global previousMotorON #was the motor on before?
 global heartBeatLed #heartbeat effect on led
 global motor
 
+# LED strip configuration:
+global LED_COUNT    # = 16 # Number of LED pixels.
+global LED_PIN      #= 18 # GPIO pin connected to the pixels (18 uses PWM!).
+global LED_FREQ_HZ # LED signal frequency in hertz (usually 800khz)
+global LED_DMA # DMA channel to use for generating signal (try 10)
+global LED_BRIGHTNESS # Set to 0 for darkest and 255 for brightest
+global LED_INVERT # True to invert the signal (when using NPN transistor level shift)
+global LED_CHANNEL# se
+global statusSTRIP
+
+
 heartBeatLed = False
 previousMotorON = False
 motorON = False
@@ -44,6 +57,16 @@ playLED = 22
 recLED = 25
 recBUT = 23
 motor = 17
+
+LED_COUNT = 5 # Number of LED pixels.
+LED_PIN = 12 # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN = 10 # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10 # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 40 # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0 # set to '1' for GPIOs 13, 19, 41, 45 or 
+statusStrip = False
 
 """
 initialisation of GPIO leds and switch and motor
@@ -64,6 +87,7 @@ async def timeC():
     global playOKD
     global recD
     global motorON
+    global statusStrip
 
     while True :
         await asyncio.sleep(0.5)
@@ -95,7 +119,9 @@ async def recTG():
             recD = 0
             pid = os.fork()
             if pid == 0 :
-                os.execl('/usr/bin/arecord','arecord','--rate=44000','/home/pi/rec.wav','')
+                #os.execl('/usr/bin/arecord','arecord','-D','plughw:1,0','--rate=44000','/home/pi/rec.wav')
+                os.execl('/usr/bin/sox','sox','-t','alsa','default','/home/pi/rec.wav')
+                #os.system('sox -t alsa default /home/pi/rec.wav')
             else:
                 while GPIO.input(recBUT) == GPIO.LOW :
                     await asyncio.sleep(delay)
@@ -107,9 +133,10 @@ async def recTG():
                 playOKD = 30
                 if recD > 1:
                     os.system('sudo killall sox')
-                    os.system('/usr/bin/sox /home/pi/rec.wav /home/pi/rec.ogg')
-                    os.rename('/home/pi/rec.ogg', '/home/pi/rec.oga')
-                    await client.send_file(peer, '/home/pi/rec.oga',voice_note=True)
+                    os.system('ffmpeg -i /home/pi/rec.wav -acodec mp3 /home/pi/rec.mp3 -y')
+                    #os.system('/usr/bin/sox /home/pi/rec.wav /home/pi/rec.ogg')
+                    #os.rename('/home/pi/rec.ogg', '/home/pi/rec.oga')
+                    await client.send_file(peer, '/home/pi/rec.mp3',voice_note=True)
         else:
             #heartBeatLed = False
             #GPIO.output(recLED, GPIO.LOW)
@@ -117,68 +144,89 @@ async def recTG():
 
 #motor uses global to turn ON the motor
 async def motor():
-	global motorON
-	global motor
-	global previousMotorON
-	# Adjust the pulse values to set rotation range
-	min_pulse = 0.000544    # Library default = 1/1000
-	max_pulse = 0.0024              # Library default = 2/1000
-	# Initial servo position
-	pos =  1
-	test = 0
-	servo = Servo(17, pos, min_pulse, max_pulse, 20/1000, None)
-	
+   global motorON
+   global motor
+   global previousMotorON
+   # Adjust the pulse values to set rotation range
+   min_pulse = 0.000544    # Library default = 1/1000
+   max_pulse = 0.0024              # Library default = 2/1000
+   # Initial servo position
+   pos =  1
+   test = 0
+   servo = Servo(17, pos, min_pulse, max_pulse, 20/1000, None)
+   
 
-	while True:
-		await asyncio.sleep(0.2)
-		if motorON == True:
-			pos=pos*(-1)
-			servo.value=pos
-			await asyncio.sleep(2)
-		else :
+   while True:
+      await asyncio.sleep(0.2)
+      if motorON == True:
+         pos=pos*(-1)
+         servo.value=pos
+         await asyncio.sleep(2)
+      else :
                         #put back in original position
                         servo.value=0
                         #detach the motor to avoid glitches and save energy
                         servo.detach()
                         previousMotorON = False
 
+
+async def LEDStripe():
+   global statusStrip
+   while True:
+      if statusStrip:
+         strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+         strip.begin()
+         i = 255
+         while(i>=0):
+            #print('1')
+
+            for j in range(strip.numPixels()):
+               strip.setPixelColor(j, Color(0,i,0))
+               strip.show()
+               asyncio.sleep(0.008)
+            i=i-1
+         print('FlaSH!')
+         await asyncio.sleep(0.3)
+      else:
+         await asyncio.sleep(0.1)
+
 #this is the les that mimic heartbeat when you have a voicemail waiting
 async def heartBeat():
-	global heartBeatLed
-	global p
+   global heartBeatLed
+   global p
 
 
-	p = GPIO.PWM(recLED, 500)     # set Frequece to 500Hz
-	p.start(100)                     # Start PWM output, Duty Cycle = 0
-	while True:
-		if heartBeatLed == True :
-			for dc in range(0, 20, 2):   # Increase duty cycle: 0~100
-				p.ChangeDutyCycle(dc)
-				await asyncio.sleep(0.01)
-			for dc in range(20, -1, -2): # Decrease duty cycle: 100~0
-				p.ChangeDutyCycle(dc)
-				await asyncio.sleep(0.005)
-			time.sleep(0.05)
-	
-			for dc in range(0, 101, 2):   # Increase duty cycle: 0~100
-				p.ChangeDutyCycle(dc)     # Change duty cycle
-				await asyncio.sleep(0.01)
-			for dc in range(100, -1, -2): # Decrease duty cycle: 100~0
-				p.ChangeDutyCycle(dc)
-				await asyncio.sleep(0.01)
+   p = GPIO.PWM(recLED, 500)     # set Frequece to 500Hz
+   p.start(100)                     # Start PWM output, Duty Cycle = 0
+   while True:
+      if heartBeatLed == True :
+         for dc in range(0, 20, 2):   # Increase duty cycle: 0~100
+            p.ChangeDutyCycle(dc)
+            await asyncio.sleep(0.01)
+         for dc in range(20, -1, -2): # Decrease duty cycle: 100~0
+            p.ChangeDutyCycle(dc)
+            await asyncio.sleep(0.005)
+         time.sleep(0.05)
+   
+         for dc in range(0, 101, 2):   # Increase duty cycle: 0~100
+            p.ChangeDutyCycle(dc)     # Change duty cycle
+            await asyncio.sleep(0.01)
+         for dc in range(100, -1, -2): # Decrease duty cycle: 100~0
+            p.ChangeDutyCycle(dc)
+            await asyncio.sleep(0.01)
 
-			await asyncio.sleep(0.06)
-	
-			for dc in range(0,8, 2):   # Increase duty cycle: 0~100
-				p.ChangeDutyCycle(dc)     # Change duty cycle
-				await asyncio.sleep(0.01)
-			for dc in range(7, -1, -1): # Decrease duty cycle: 100~0
-				p.ChangeDutyCycle(dc)
-				await asyncio.sleep(0.01)
-			await asyncio.sleep(1)
-			
-		else :
-                        await asyncio.sleep(0.1)
+         await asyncio.sleep(0.06)
+   
+         for dc in range(0,8, 2):   # Increase duty cycle: 0~100
+            p.ChangeDutyCycle(dc)     # Change duty cycle
+            await asyncio.sleep(0.01)
+         for dc in range(7, -1, -1): # Decrease duty cycle: 100~0
+            p.ChangeDutyCycle(dc)
+            await asyncio.sleep(0.01)
+         await asyncio.sleep(1)
+         
+      else :
+            await asyncio.sleep(0.1)
 
 
 
@@ -194,6 +242,7 @@ async def playTG():
     global motorON
     global heartBeatLed
     global servo
+    global statusStrip
 
     playing = 0
     while True:
@@ -201,11 +250,13 @@ async def playTG():
             GPIO.output(playLED, GPIO.HIGH)
             motorON = True
             heartBeatLed = True
+            statusStrip = True
             
         else:
             GPIO.output(playLED, GPIO.LOW)
             motorON = False
             heartBeatLed = False
+            statusStrip = False
 
             
         if (toPlay >= 0) and (playOK == True):
@@ -241,39 +292,22 @@ download of file .oga renamed .ogg
 """
 GPIO.output(playLED, GPIO.HIGH)
 motorON=True
-api_id = 592944
-api_hash = 'ae06a0f0c3846d9d4e4a7065bede9407'
-client =  TelegramClient('session_name', api_id, api_hash)
-asyncio.sleep(2)
+api_id = 1105416
+api_hash = '2bc41ff3e4672dd00f1efef7f52e0d87'
+client =  TelegramClient('LoveCats', api_id, api_hash)
+time.sleep(2)
 client.connect()
 if not  client.is_user_authorized():
-    while os.path.exists('/home/pi/phone') == False:
-        pass
-    f = open('/home/pi/phone', 'r')
-    phone = f.read()
-    f.close()
-    #os.remove('/home/pi/phone')
-    print(phone)
-
-    asyncio.sleep(2)
+    time.sleep(2)
     client.send_code_request(phone,force_sms=True)
-
-    while os.path.exists('/home/pi/key') == False:
-        pass
-    f = open('/home/pi/key', 'r')
-    key = f.read()
-    f.close()
+    key = input('Enter key: ')
     print (key)
-    os.remove('/home/pi/key')
     asyncio.sleep(2)
     me = client.sign_in(phone=phone, code=key)
 GPIO.output(playLED, GPIO.LOW)        
 motorON=False
+ 
 
-p = open('/boot/PEER.txt','r')
-peer = p.readline() 
-if peer[-1] == '\n':
-    peer = peer[0:-1]
 #print(peer)
 #print(len(peer))
 @client.on(events.NewMessage)
@@ -291,7 +325,9 @@ async def receiveTG(event):
             #print(toPlay)
             if toPlay == 0:
                 #os.system('/usr/bin/cvlc --play-and-exit /home/pi/LB/lovebird.wav')
-                os.system('/usr/bin/cvlc --play-and-exit /home/pi/LB/lovebird.wav')
+                sounds =  os.listdir('./sounds/')
+                print(sounds)
+                os.system('/usr/bin/cvlc --play-and-exit ./sounds/'+sounds[randrange(len(sounds))])
             name = '/home/pi/play' + str(toPlay) +  '.ogg'
             #print(name)
             os.rename(ad,name)
@@ -303,7 +339,7 @@ Main sequence (handler receiveTG), playTG, timeC, recTG, motor et heartBeat are 
 
 """
 #os.system('/usr/bin/cvlc /home/pi/LB/lovebird.wav vlc://quit')
-os.system('/usr/bin/cvlc --play-and-exit /home/pi/LB/lovebird.wav')
+os.system('/usr/bin/cvlc --play-and-exit ./sounds/cat_purr.wav')
 
 loop = asyncio.get_event_loop()
 loop.create_task(recTG())
@@ -311,6 +347,9 @@ loop.create_task(playTG())
 loop.create_task(timeC())
 loop.create_task(motor())
 loop.create_task(heartBeat())
+loop.create_task(LEDStripe())
 loop.run_forever()
 client.run_until_disconnected()
+
+
 
